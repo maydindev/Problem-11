06dcecb89c78a528d4d8a772f4d5c54c05ea3234
maydindev <maydin.dev@outlook.com>
2024-07-31 12:38:57 +0300

    Updated.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
 import { CameraIcon, VideoCameraIcon } from "@heroicons/react/24/outline";
+import { useRef } from "react";
 
 // Bir "BoxButton" bileşeni oluşturun ve farklı içerik iletmek için children prop'unu kullanın
 function BoxButton({ children }) {
+  const fileInputRef = useRef(null);
+
+  const handleButtonClick = () => {
+    fileInputRef.current.click();
+  };
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      console.log(`Selected file: ${file.name}`);
+      // Dosya ile ilgili işlemler de burada yapılabilir.
+    }
+  };
   return (
     //<button>{children}</button>
-    <button
-      type="button"
-      className="relative block w-full rounded-lg border-2 border-dashed border-gray-300 p-12 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-    >
-      {children}
-    </button>
+    <>
+      <button
+        onClick={handleButtonClick}
+        type="button"
+        className="relative block w-full rounded-lg border-2 border-dashed border-gray-300 p-12 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+      >
+        {children}
+      </button>
+      <input
+        type="file"
+        ref={fileInputRef}
+        style={{ display: "none" }}
+        onChange={handleFileChange}
+      />
+    </>
   );
 }
 
@@ -31,26 +54,3 @@ export default function App() {
     </div>
   );
 }
-
-
-
-/*
-<button
-        type='button'
-        className='relative block w-full rounded-lg border-2 border-dashed border-gray-300 p-12 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2'
-      >
-        <CameraIcon className='mx-auto h-12 w-12 text-gray-400' />
-        <span className='mt-2 block text-sm font-semibold text-gray-900'>
-          Yeni bir resim ekleyin
-        </span>
-      </button>
-      <button
-        type='button'
-        className='relative block w-full rounded-lg border-2 border-dashed border-gray-300 p-12 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2'
-      >
-        <VideoCameraIcon className='mx-auto h-12 w-12 text-gray-400' />
-        <span className='mt-2 block text-sm font-semibold text-gray-900'>
-          Yeni bir video oluşturun
-        </span>
-      </button>
-      */
